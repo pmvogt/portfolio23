50606bf56854758f182db45c92acd9bca5b6f52f
peter vogt <peter@petervogt.co>
2023-12-17 16:20:58 -0600

    removes spline-viewer

--- app/components/window.tsx
+++ app/components/window.tsx
@@ -1,56 +1,55 @@
-'use client';
+"use client";
 
-import { Flex, Text } from '@radix-ui/themes';
-import SplineViewer from './splineviewer';
+import { Flex, Text } from "@radix-ui/themes";
+import SplineViewer from "./splineviewer";
 
 export default function Window() {
-  return (
-    <Flex
-      align="center"
-      direction="column"
-      content="center"
-      py="9"
-      width="100%"
-      height="100%"
-    >
-      <Flex
-        py="5"
-        pl="4"
-        width="100%"
-        style={{
-          backgroundColor: 'var(--neutral-a12)',
-          borderTopLeftRadius: 4,
-          borderTopRightRadius: 4,
-          borderWidth: 1,
-          borderStyle: 'solid',
-          borderBottomColor: 'transparent',
-          borderColor: 'var(--neutral-a11)',
-          color: 'var(--neutral-11)',
-        }}
-      >
-        <Text
-          size="2"
-          trim="both"
-          style={{ fontFamily: 'var(--font-geist-mono)' }}
-        >
-          effects/gradient
-        </Text>
-      </Flex>
-      <Flex
-        width="100%"
-        mt="0"
-        style={{
-          backgroundColor: 'var(--gold-7)',
-          borderWidth: 1,
-          borderBottomLeftRadius: 2,
-          borderBottomRightRadius: 2,
-          borderStyle: 'solid',
-          borderColor: 'var(--gold-7)',
-        }}
-      >
-        test
-        {/* <SplineViewer /> */}
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex
+			align="center"
+			direction="column"
+			content="center"
+			py="9"
+			width="100%"
+			height="100%"
+		>
+			<Flex
+				py="5"
+				pl="4"
+				width="100%"
+				style={{
+					backgroundColor: "var(--neutral-a12)",
+					borderTopLeftRadius: 4,
+					borderTopRightRadius: 4,
+					borderWidth: 1,
+					borderStyle: "solid",
+					borderBottomColor: "transparent",
+					borderColor: "var(--neutral-a11)",
+					color: "var(--neutral-11)",
+				}}
+			>
+				<Text
+					size="2"
+					trim="both"
+					style={{ fontFamily: "var(--font-geist-mono)" }}
+				>
+					effects/gradient
+				</Text>
+			</Flex>
+			<Flex
+				width="100%"
+				mt="0"
+				style={{
+					backgroundColor: "var(--gold-7)",
+					borderWidth: 1,
+					borderBottomLeftRadius: 2,
+					borderBottomRightRadius: 2,
+					borderStyle: "solid",
+					borderColor: "var(--gold-7)",
+				}}
+			>
+				content
+			</Flex>
+		</Flex>
+	);
 }
