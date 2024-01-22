6d14663660c9e6ba0abec82054e12de5a69bbe8a
peter vogt <peter@petervogt.co>
2024-01-22 16:39:14 -0600

    adds layout density selector

--- app/components/window.tsx
+++ app/components/window.tsx
@@ -1,59 +1,104 @@
 "use client";
-import { Badge, Box, Flex, Text } from "@radix-ui/themes";
+import { useState } from "react";
+import {
+  Badge,
+  Container,
+  Flex,
+  Text,
+  DropdownMenu,
+  IconButton,
+} from "@radix-ui/themes";
 
 import Spline from "@splinetool/react-spline";
-import { FigmaLogoIcon, HeartFilledIcon } from "@radix-ui/react-icons";
+import {
+  CaretDownIcon,
+  ColumnSpacingIcon,
+  FigmaLogoIcon,
+  HeartFilledIcon,
+} from "@radix-ui/react-icons";
 
 import ColorBar from "./colorbar";
 
 export default function Window() {
-  return (
-    <Flex direction="column" gap="3" width="100%">
-      <Flex pt="8" align="end" justify="between">
-        <Flex direction="column">
-          <Text style={{ color: "var(--accent-a12)" }} size="3" weight="bold">
-            Peter Vogt
-          </Text>
-        </Flex>
-        <Flex>
-          <Text
-            style={{
-              color: "var(--neutral-a09)",
-            }}
-            size="2"
-          >
-            Design Engineer
-          </Text>
-        </Flex>
-      </Flex>
-      <Spline scene="https://prod.spline.design/ozb1e9hCLqo73g6G/scene.splinecode" />
-      <Flex
-        justify={{ initial: "center", md: "between" }}
-        align="center"
-        width="100%"
-      >
-        <Flex
-          display={{
-            initial: "none",
-            md: "flex",
-          }}
-        >
-          <Badge size="1" highContrast variant="surface" color="gray">
-            <FigmaLogoIcon width="16" height="16" />
-            100
-            <HeartFilledIcon width="16" height="16" />
-          </Badge>
-        </Flex>
+  const [density, setDensity] = useState("1");
 
+  const handleDensityChange = (newDensity) => {
+    setDensity(newDensity);
+  };
+
+  return (
+    <Container size={density} height="100%" width="100%" px="3">
+      <Flex direction="column" gap="3" width="100%">
+        <Flex pt="6" align="end" justify="between">
+          <Flex direction="column">
+            <Text style={{ color: "var(--accent-a12)" }} size="3" weight="bold">
+              Peter Vogt
+            </Text>
+            <Text
+              style={{
+                color: "var(--gray-10)",
+              }}
+              size="2"
+            >
+              Design Engineer
+            </Text>
+          </Flex>
+          <Flex>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <IconButton variant="surface" highContrast>
+                  <ColumnSpacingIcon />
+                </IconButton>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content size="1">
+                <DropdownMenu.Label>Layout Density</DropdownMenu.Label>
+                <DropdownMenu.Item onSelect={() => handleDensityChange("1")}>
+                  Compact
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onSelect={() => handleDensityChange("3")}>
+                  High
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onSelect={() => handleDensityChange("2")}>
+                  Low
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </Flex>
+        </Flex>
+        <Spline scene="https://prod.spline.design/ozb1e9hCLqo73g6G/scene.splinecode" />
         <Flex
-          display={{
-            initial: "none",
-            md: "flex",
-          }}
+          justify={{ initial: "center", md: "between" }}
+          align="center"
+          width="100%"
         >
-          <ColorBar />
+          <Flex
+            display={{
+              initial: "none",
+              md: "flex",
+            }}
+          >
+            <Badge size="1" highContrast variant="surface" color="gray">
+              <FigmaLogoIcon width="16" height="16" />
+              100
+              <HeartFilledIcon width="16" height="16" />
+            </Badge>
+          </Flex>
+
+          <Flex
+            display={{
+              initial: "none",
+              md: "flex",
+            }}
+          >
+            <ColorBar />
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+      <Text>
+        Software designer who loves writing code. I like sweating interaction
+        details, automating the work of design teams, and building beautiful
+        interfaces that scale well.
+      </Text>
+    </Container>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Flex, Container } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
 import Window from "./components/window";
 
 export default function Home() {
@@ -8,11 +8,7 @@ export default function Home() {
       pb={{ initial: "0", md: "9" }}
       px={{ initial: "0", md: "4" }}
     >
-      <Container size="1" height="100%" width="100%" px="3">
-        <Flex direction="column">
-          <Window />
-        </Flex>
-      </Container>
+      <Window />
     </Flex>
   );
 }
